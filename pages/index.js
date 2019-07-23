6c68e5d2b22195e33b45b27e689ab031e8ec9174
Cole Challand <ctchalland@gmail.com>
2019-07-23 01:02:34 -0500

    Adding axios and starting the index's component

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class BetokenIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blockchain: {},
+      games: {}
+    };
+  }
+
+  async componentDidMount() {}
+}
