0b28957b2cf8b8dd3a8941e0054dc4acbec30651
Cole Challand <ctchalland@gmail.com>
2019-08-08 18:22:58 -0500

    Trying to change the png file names

--- static/rename.py
+++ static/rename.py
@@ -2,7 +2,6 @@ import os
 
 
 def main():
-    i = 0
 
     for filename in os.listdir("media"):
         if filename[0:2] == "mlb":
@@ -61,6 +60,6 @@ def main():
 
             os.rename(src, dst)
 
-    if __name__ == '__main__':
 
-        main()
+if __name__ == '__main__':
+    main()
