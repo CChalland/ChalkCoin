8bddd6b1fd983d9b9141b2d14738a9acef4271d2
Cole Challand <ctchalland@gmail.com>
2019-08-09 09:22:35 -0500

    Updated the renaming script

--- static/rename.py
+++ static/rename.py
@@ -1,60 +1,67 @@
 import os
 
-
 def main():
 
     for filename in os.listdir("media"):
         if filename[0:3] == "mlb":
-            dst = "3" + filename[3:]
+            char = filename.find('.')
+            dst = "3" + filename[3:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
             os.rename(src, dst)
 
-        if filename[0:3] == "mls":
-            dst = "10" + filename[3:]
+        elif filename[0:3] == "mls":
+            char = filename.find('.')
+            dst = "10" + filename[3:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
             os.rename(src, dst)
 
-        if filename[0:3] == "nba":
-            dst = "4" + filename[3:]
+        elif filename[0:3] == "nba":
+            char = filename.find('.')
+            dst = "4" + filename[3:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
             os.rename(src, dst)
 
-        if filename[0:5] == "ncaab":
-            dst = "5" + filename[5:]
+        elif filename[0:5] == "ncaab":
+            char = filename.find('.')
+            dst = "5" + filename[5:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
             os.rename(src, dst)
 
-        if filename[0:5] == "ncaaf":
-            dst = "1" + filename[5:]
+        elif filename[0:5] == "ncaaf":
+            char = filename.find('.')
+            dst = "1" + filename[5:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
             os.rename(src, dst)
 
-        if filename[0:3] == "nfl":
-            dst = "2" + filename[3:]
+        elif filename[0:3] == "nfl":
+            char = filename.find('.')
+            dst = "2" + filename[3:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
             os.rename(src, dst)
 
-        if filename[0:3] == "nhl":
-            dst = "6" + filename[3:]
+        elif filename[0:3] == "nhl":
+            char = filename.find('.')
+            dst = "6" + filename[3:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
             os.rename(src, dst)
 
-        if filename[0:4] == "wnba":
-            dst = "8" + filename[4:]
+        elif filename[0:4] == "wnba":
+            char = filename.find('.')
+            dst = "8" + filename[4:char] + ".png"
             src = "media/" + filename
             dst = "media/" + dst
 
