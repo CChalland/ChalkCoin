9800029acb33175ef1d60ef6dcf54359da21d5f3
Cole Challand <ctchalland@gmail.com>
2019-08-14 15:45:01 -0500

    Installed npm  package

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fbce669651eaafe1ccfe";
+/******/ 	var hotCurrentHash = "feb82fdd0a50f647c018";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
