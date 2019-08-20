1c121b443aba17441573f29e1f8d2778bb5dbc86
Cole Challand <ctchalland@gmail.com>
2019-08-20 08:17:08 -0500

    Added an Array prototype function to subtract the same elements out when comparing two arrays. This is for not rendering the sport tabs that have no event data

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eecc72612fa9ec2a62b8";
+/******/ 	var hotCurrentHash = "d9422f98b6729edd1394";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,17 @@ import BetContextProvider from "../components/provider/BetContextProvider";
 
 class BetokenIndex extends Component {
   static async getInitialProps() {
-    let activeIndex = 0;
+    let removeSportsData = [];
     let sportsData = [
       { sport_id: 1, sport_name: "NCAA Football", data: {} },
       { sport_id: 2, sport_name: "NFL", data: {} },
       { sport_id: 3, sport_name: "MLB", data: {} },
-      /*{ sport_id: 4, sport_name: "NBA", data: {} },
+      { sport_id: 4, sport_name: "NBA", data: {} },
       { sport_id: 5, sport_name: "NCAA Men's Basketball", data: {} },
       { sport_id: 6, sport_name: "NHL", data: {} },
       { sport_id: 7, sport_name: "UFC/MMA", data: {} },
       { sport_id: 8, sport_name: "WNBA", data: {} },
-      { sport_id: 9, sport_name: "CFL", data: {} },*/
+      { sport_id: 9, sport_name: "CFL", data: {} },
       { sport_id: 10, sport_name: "MLS", data: {} }
     ];
     const getNode1 = `http://localhost:3001/blockchain`;
@@ -47,10 +47,14 @@ class BetokenIndex extends Component {
           }
         }).then(
           function(response) {
-            sportsData[i].data = response.data;
-            sportsData[i].data.events = response.data.events.sort(function(a, b) {
-              return new Date(a.event_date) - new Date(b.event_date);
-            });
+            if (response.data.events.length === 0) {
+              removeSportsData.push(sportsData[i]);
+            } else {
+              sportsData[i].data = response.data;
+              sportsData[i].data.events = response.data.events.sort(function(a, b) {
+                return new Date(a.event_date) - new Date(b.event_date);
+              });
+            }
             console.log(sportsData[i].data);
           }.bind(this)
         );
@@ -59,7 +63,16 @@ class BetokenIndex extends Component {
       console.log(err.message);
     }
 
-    return { activeIndex, sportsData, blockchain };
+    Array.prototype.diff = function(a) {
+      return this.filter(function(i) {
+        return a.indexOf(i) < 0;
+      });
+    };
+
+    sportsData = sportsData.diff(removeSportsData);
+    console.log(sportsData);
+
+    return { sportsData, blockchain };
   }
 
   renderCurrentBets() {
