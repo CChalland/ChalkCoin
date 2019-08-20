cba0dca1a934b2a0a5a0467c147ded182962a963
Cole Challand <ctchalland@gmail.com>
2019-08-20 15:59:53 -0500

    Added page-layout-temps in gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "afac4c207c33b5e7b4d6";
+/******/ 	var hotCurrentHash = "865514da256f6de4644f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
