36acbc317cadb72c2bfbe1b149885e52d9029ebb
Cole Challand <ctchalland@gmail.com>
2019-08-23 15:17:46 -0500

    Updated the props

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b48fee7da7fe6d3434e2";
+/******/ 	var hotCurrentHash = "f80eb0748fbb04673996";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/EventCard.js
+++ components/EventCard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class EventCard extends Component {
   render() {
-    console.log("EventCard render: ", this.props);
+    console.log("EventCard render: ", this.props.eventsData);
     return (
       <div>
         <h3>Event Details...</h3>
