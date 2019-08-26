d9cf948b742056f6758e09966847bc43e6e2b641
Cole Challand <ctchalland@gmail.com>
2019-08-26 09:41:46 -0500

    removed some code that was uneeded

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f73efe0640c107c296ab";
+/******/ 	var hotCurrentHash = "8a8ae244abacb77ef558";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
