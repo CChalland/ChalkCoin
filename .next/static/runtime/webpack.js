58f3368b0cf6d0a448a075c2e5a7cb590815543a
Cole Challand <ctchalland@gmail.com>
2019-09-07 13:04:24 -0500

    Added NHL and MLS team colors

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d39440081cbb18ba8a04";
+/******/ 	var hotCurrentHash = "1dcfc68af3c062c5a58e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
