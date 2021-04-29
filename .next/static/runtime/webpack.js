62968bfc66059ffc60cf3a9cf17b41d81bacc6f3
Cole Challand <ctchalland@gmail.com>
2021-04-29 09:01:34 -0400

    change gamescoretable to a class

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "06c7ba61bb45c03c58d1";
+/******/ 	var hotCurrentHash = "fa4a3fd6b089d9ffde55";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/GameScoreTable.js
+++ components/GameScoreTable.js
@@ -1,80 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
 import { Header, Table, Rating } from "semantic-ui-react";
 
-const GameScoreTable = () => (
-	<Table celled padded>
-		<Table.Header>
-			<Table.Row>
-				<Table.HeaderCell singleLine>shortDetail</Table.HeaderCell>
-				<Table.HeaderCell>linescore1</Table.HeaderCell>
-				<Table.HeaderCell>linescore2</Table.HeaderCell>
-				<Table.HeaderCell>linescore3</Table.HeaderCell>
-				<Table.HeaderCell>linescore4</Table.HeaderCell>
-				<Table.HeaderCell>OT</Table.HeaderCell>
-				<Table.HeaderCell>T</Table.HeaderCell>
-			</Table.Row>
-		</Table.Header>
+class GameScoreTable extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-		<Table.Body>
-			<Table.Row>
-				<Table.Cell>
-					<Header as="h2" textAlign="center">
-						<img
-							className="ui avatar image"
-							src={"https://a.espncdn.com/i/teamlogos/nba/500/scoreboard/det.png"}
-						/>
-						{"team.name"}
-						{"( "}
-						{"records[0].summary"}
-						{", "}
-						{"records[2].summary"} {"homeAway"}
-						{" )"}
-					</Header>
-				</Table.Cell>
-				<Table.Cell singleLine>Power Output</Table.Cell>
-				<Table.Cell>
-					<Rating icon="star" defaultRating={3} maxRating={3} />
-				</Table.Cell>
-				<Table.Cell textAlign="right">
-					80% <br />
-					<a href="#">18 studies</a>
-				</Table.Cell>
-				<Table.Cell>
-					Creatine supplementation is the reference compound for increasing muscular creatine levels; there is
-					variability in this increase, however, with some nonresponders.
-				</Table.Cell>
-			</Table.Row>
+	renderScoreTable() {
+		return (
+			<Table celled padded>
+				<Table.Header>
+					<Table.Row>
+						<Table.HeaderCell singleLine>shortDetail</Table.HeaderCell>
+						<Table.HeaderCell>linescore1</Table.HeaderCell>
+						<Table.HeaderCell>linescore2</Table.HeaderCell>
+						<Table.HeaderCell>linescore3</Table.HeaderCell>
+						<Table.HeaderCell>linescore4</Table.HeaderCell>
+						<Table.HeaderCell>OT</Table.HeaderCell>
+						<Table.HeaderCell>T</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
 
-			<Table.Row>
-				<Table.Cell>
-					<Header as="h2" textAlign="center">
-						<img
-							className="ui avatar image"
-							src={"https://a.espncdn.com/i/teamlogos/nba/500/scoreboard/cle.png"}
-						/>
-						{"team.name"}
-						{"( "}
-						{"records[0].summary"}
-						{", "}
-						{"records[2].summary"} {"homeAway"}
-						{" )"}
-					</Header>
-				</Table.Cell>
-				<Table.Cell singleLine>Weight</Table.Cell>
-				<Table.Cell>
-					<Rating icon="star" defaultRating={3} maxRating={3} />
-				</Table.Cell>
-				<Table.Cell textAlign="right">
-					100% <br />
-					<a href="#">65 studies</a>
-				</Table.Cell>
-				<Table.Cell>
-					Creatine is the reference compound for power improvement, with numbers from one meta-analysis to
-					assess potency
-				</Table.Cell>
-			</Table.Row>
-		</Table.Body>
-	</Table>
-);
+				<Table.Body>
+					<Table.Row>
+						<Table.Cell>
+							<Header as="h2" textAlign="center">
+								<img
+									className="ui avatar image"
+									src={"https://a.espncdn.com/i/teamlogos/nba/500/scoreboard/det.png"}
+								/>
+								{"team.name"}
+								{"( "}
+								{"records[0].summary"}
+								{", "}
+								{"records[2].summary"} {"homeAway"}
+								{" )"}
+							</Header>
+						</Table.Cell>
+						<Table.Cell singleLine>Power Output</Table.Cell>
+						<Table.Cell>
+							<Rating icon="star" defaultRating={3} maxRating={3} />
+						</Table.Cell>
+						<Table.Cell textAlign="right">
+							80% <br />
+							<a href="#">18 studies</a>
+						</Table.Cell>
+						<Table.Cell>
+							Creatine supplementation is the reference compound for increasing muscular creatine levels;
+							there is variability in this increase, however, with some nonresponders.
+						</Table.Cell>
+					</Table.Row>
+
+					<Table.Row>
+						<Table.Cell>
+							<Header as="h2" textAlign="center">
+								<img
+									className="ui avatar image"
+									src={"https://a.espncdn.com/i/teamlogos/nba/500/scoreboard/cle.png"}
+								/>
+								{"team.name"}
+								{"( "}
+								{"records[0].summary"}
+								{", "}
+								{"records[2].summary"} {"homeAway"}
+								{" )"}
+							</Header>
+						</Table.Cell>
+						<Table.Cell singleLine>Weight</Table.Cell>
+						<Table.Cell>
+							<Rating icon="star" defaultRating={3} maxRating={3} />
+						</Table.Cell>
+						<Table.Cell textAlign="right">
+							100% <br />
+							<a href="#">65 studies</a>
+						</Table.Cell>
+						<Table.Cell>
+							Creatine is the reference compound for power improvement, with numbers from one meta-analysis to
+							assess potency
+						</Table.Cell>
+					</Table.Row>
+				</Table.Body>
+			</Table>
+		);
+	}
+
+	render() {
+		return <div>{this.renderScoreTable()}</div>;
+	}
+}
 
 export default GameScoreTable;
