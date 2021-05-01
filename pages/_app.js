1260707a70a9ba7446aeba32e977d255c501c6c5
Cole Challand <ctchalland@gmail.com>
2021-04-30 20:07:30 -0400

    updating npm

--- pages/_app.js
+++ pages/_app.js
@@ -3,26 +3,26 @@ import App from "next/app";
 import { SportProvider } from "../contexts/SportContext";
 
 class MyApp extends App {
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // static async getInitialProps(appContext) {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
+	// Only uncomment this method if you have blocking data requirements for
+	// every single page in your application. This disables the ability to
+	// perform automatic static optimization, causing every page in your app to
+	// be server-side rendered.
+	//
+	// static async getInitialProps(appContext) {
+	//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+	//   const appProps = await App.getInitialProps(appContext);
+	//
+	//   return { ...appProps }
+	// }
 
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <SportProvider>
-        <Component {...pageProps} />
-      </SportProvider>
-    );
-  }
+	render() {
+		const { Component, pageProps } = this.props;
+		return (
+			<SportProvider>
+				<Component {...pageProps} />
+			</SportProvider>
+		);
+	}
 }
 
 export default MyApp;
