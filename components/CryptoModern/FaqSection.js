dbc284f24bc11844ea3a6c2e73ce6f88fcaca449
Cole Challand <ctchalland@gmail.com>
2021-11-29 10:46:30 -0500

    editting layout for index page

--- components/CryptoModern/FaqSection.js
+++ components/CryptoModern/FaqSection.js
@@ -9,19 +9,19 @@ export default function FaqSection() {
 			expend: true,
 			title: "How to contact with Customer Service?",
 			description:
-				"Our Customer Experience Team is available 7 days a week and we offer 2 ways to get in contact. Email and Chat. We try to reply quickly, so you need not to wait too long for a response!. ",
+				"Our Customer Experience Team is available 7 days a week. We try to reply quickly, so you need not to wait too long for a response!.",
 		},
 		{
 			id: 2,
 			title: "App installation failed, how to update system information?",
 			description:
-				"Please read the documentation carefully. We also have some online video tutorials regarding this issue. If the problem remains, Please Open a ticket in the support forum. ",
+				"Please read the documentation carefully. If the problem remains, Please Open a ticket in the support forum. ",
 		},
 		{
 			id: 3,
 			title: "Website reponse taking time, how to improve?",
 			description:
-				"At first, Please check your internet connection. We also have some online video tutorials regarding this issue. If the problem remains, Please Open a ticket in the support forum.",
+				"At first, Please check your internet connection. If the problem remains, Please Open a ticket in the support forum.",
 		},
 		{
 			id: 4,
@@ -35,15 +35,15 @@ export default function FaqSection() {
 		<Container>
 			<Row className="justify-content-center">
 				<Col xs={"auto"}>
-					<h1>Have Any Question ?</h1>
+					<h1>Have Any Question?</h1>
 				</Col>
 			</Row>
-			<Row className="justify-content-center">
+			<Row className="justify-content-center mb-3">
 				<Col xs={"auto"}>
-					<h5 className="text-secondary">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiu Lorem ipsum dolor Lorem ipsum
-						dolor
-					</h5>
+					<div className="card-category form-category text-secondary">
+						<span className="star">* </span>
+						Please keep in mind that this is a personal project and has no company backing it.
+					</div>
 				</Col>
 			</Row>
 
