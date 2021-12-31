7dddbbe22ceb1f34759a63b0dbd43881f7b12a57
Cole Challand <ctchalland@gmail.com>
2021-12-31 07:09:00 -0500

    fixed localhost blockchain not connecting to site

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -15,7 +15,7 @@ var corsOptions = {
 const app = express();
 const chalkCoin = new Blockchain();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
